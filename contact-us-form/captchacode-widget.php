01bb9f09f982b08efce3e684f67db50f3000dd56
KNisansala <kavini@sublime.lk>
2018-08-29 09:53:50 +0530

    #40 Design Corrections of Contacts Page

--- contact-us-form/captchacode-widget.php
+++ contact-us-form/captchacode-widget.php
@@ -2,12 +2,12 @@
 $captchacode = rand();
 ?>               
 
-<img src="booking-form/Generate_Captcha_Code.php?rand=<?php echo $captchacode; ?>" id='captchaimg'  style=" margin-top: 17px;">  
+<img src="contact-us-form/Generate_Captcha_Code.php?rand=<?php echo $captchacode; ?>" id='captchaimg'  style=" margin-top: 30px;">  
 
 <a href='javascript: refreshCaptcha();' class="contact-detail">
     <div class="refreshbox">
         <div class="refresh-img">        
-            <img style="border:none;" src="booking-form/img/refresh.png" title="Click to change the code"/>
+            <img style="border:none;" src="contact-us-form/img/refresh.png" title="Click to change the code"/>
         </div>
     </div>
 </a>
